9dd16fd350507dc9303b480dc998ed6b8827058f
Asif2535 <aiqbal517@gmail.com>
2021-03-24 00:24:43 +0600

    learned about post method

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
 
 const users = ['Asad','Moin','Sabed','Susmita','Sohana','Sabana'];
 
 app.get('/', (req,res)=>{
     const fruit = {
-        product: 'ada',
+        product:'ada',
         price:220
     }
     res.send(fruit);
 });
 
-app.get('/fruits/banana',(req, res)=>{
+app.get('/fruits/banana', (req, res)=>{
     res.send({fruit:'banana', quantity:1000, price:10000});
 });
 
 app.get('/users/:id',(req, res)=>{
-    const id = req.params.id;
-    console.log(req.query.sort);
-    const name = users[id];
-    res.send({id, name});
+    const Id = req.params.id;
+    // console.log(req.query.sort); //http://localhost:3000/users/5?sort=desc
+    const name = users[Id];
+    res.send({Id, name});
+})
+
+// post
+app.post('/addUser', (req, res)=>{
+    // save to database
+    
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
 })
 
 app.listen(3000, ()=>console.log('Listenting to port 3000'));
